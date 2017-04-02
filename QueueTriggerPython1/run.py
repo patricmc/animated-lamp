6311a329637b2182025d0edc4c4f98324aec8692
Patric McElroy <patricmc@microsoft.com>
2017-04-01 17:00:37 -0700

    small change

--- QueueTriggerPython1/run.py
+++ QueueTriggerPython1/run.py
@@ -2,5 +2,7 @@ import os
 
 # read the queue message and write to stdout
 inputMessage = open(os.environ['inputMessage']).read()
-message = "Hello world!  Python script processed queue message '{0}'".format(inputMessage)
+msgCnt = len(inputMessage)
+message = "Hello world!  Processed queue message '{0}'".format(inputMessage)
 print(message)
+print("Length '{0}".format(msgCnt + ""))
